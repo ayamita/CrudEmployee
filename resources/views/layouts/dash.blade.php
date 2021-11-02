7e534875134a9535fd95b5800d828928493c4f83
Angel Yam <amgelyam@gmail.com>
2021-11-01 22:48:39 -0600

    change route config css

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -5,8 +5,8 @@
   <title>
   </title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" >
-  <link rel="stylesheet" type="text/css"  href="{{ secure_assets('css/app.css') }}" />
-  <script src="{{ secure_assets('js/jquery-3.6.0.min.js') }}"></script>
+  <link rel="stylesheet" type="text/css"  href="{{ asset('css/app.css') }}" />
+  <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
 
 
   <script src="https://unpkg.com/boxicons@latest/dist/boxicons.js"></script>

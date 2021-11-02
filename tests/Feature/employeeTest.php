7fecf2256ca86344e6bbf13094f3b8c64506a2df
Angel Yam <amgelyam@gmail.com>
2021-11-02 11:31:34 -0600

    Test error

--- tests/Feature/employeeTest.php
+++ tests/Feature/employeeTest.php
@@ -6,17 +6,33 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class employeeTest extends TestCase
+class EmployeeTest extends TestCase
 {
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_getemployee()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function a_post_can_be_create()
+    {
+        $response = $this->post('/posts',[
+            'name' => 'Pablo',
+            'email' => 'pablo@gmail.com',
+            'admission_date' => '2000-02-24'
+        ]);
+
+        $response->assertOk();
+        $this->assertCount(1, Post::all());
+
+        $post = Post::first();
+
+        $this->assertEquals($post->name, 'Pablo');
     }
 }
